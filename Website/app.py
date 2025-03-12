5ede3a25362ea34ae845e34f719e44d88999da5c
Das_F1sHy312 <86548888+f1shyondrugs@users.noreply.github.com>
2025-03-12 19:26:58 +0100

    robots.txt + SEO Relevance + Metadata

--- Website/app.py
+++ Website/app.py
@@ -72,5 +72,9 @@ def download_file(platform):
 def index():
     return render_template('index.html')
 
+@app.route('/robots.txt')
+def robots():
+    return send_from_directory('.', 'robots.txt')
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
